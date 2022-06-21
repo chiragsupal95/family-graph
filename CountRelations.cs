15d2c7f64a415afcd363377af361b67ec97cd968
chirag <chirag.supal@mobiuso.com>
2022-06-21 11:43:47 +0530

    Refactored the code. Tried to handle operations with objects

---
+++ CountRelations.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FamilyGraph
+{
+    public class CountRelations
+    {
+        private string name;
+        private int count;
+        
+
+        public CountRelations(string name, int count)
+        {
+            this.name = name;
+            this.count = count;
+            
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Count
+        {
+            get { return count; }
+            set { count = value; }
+        }
+        
+    }
+}
